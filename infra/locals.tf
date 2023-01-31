c26223aa05541c80f5a46700d87fa08958f60f82
jus-ty <justinwalton48@gmail.com>
2023-01-31 20:56:36 +1100

    new tag and region

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,3 +1,3 @@
 locals {
-    info_tags = "${merge(var.generic_tags, {"Branch": "${var.git_branch}"})}"
+    info_tags = "${merge(var.generic_tags, {"Branch": "${var.git_branch}"}, {"Project": "${var.bucket_name}"})}"
 }
